7e28db7a7fa5ee1228662c65c722865654b68d16
Zj <6811130054@mut.ac.th>
2025-11-25 17:28:19 +0700

    การทดลองครั้งที่2

---
+++ LAB02/TEST01-1/TEST01-1.cpp
@@ -0,0 +1,14 @@
+// C++
+#include <iostream>
+
+int main() {
+
+
+    std::cout << "Hello World!\n";
+    std::cout << "Hello LAB2!\n";
+
+    std::cout << "Name: Chetthida Supapol" << std::endl;
+    std::cout << "Course: MIIA0106" << std::endl;
+    std::cout << "Email: 6811130054@mut.ac.th" << std::endl;
+    return 0;
+}
